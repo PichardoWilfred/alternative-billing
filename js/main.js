d3910cc1048f8d921224dea885d7961087030d31
Developer <pichardowilfred@gmail.com>
2023-03-03 17:56:39 -0400

    tag and select functionality, responsive design

--- js/main.js
+++ js/main.js
@@ -11,9 +11,21 @@ document.addEventListener('alpine:init', () => { //need this event listener to b
         }
     })
 })
+function focusDiv(id, product_id) {
+    const product_element = document.querySelector(`#product-${product_id}`);
+    let p = product_element.querySelector(id);
+    console.log(p);
+    let s = window.getSelection()
+    let r = document.createRange()
+    r.setStart(p, p.childElementCount)
+    r.setEnd(p, p.childElementCount)
+    s.removeAllRanges()
+    s.addRange(r)
+}
 
 document.addEventListener('alpine:init', () => {
     Alpine.data('store', () => ({
+        focus_timeout: 0,
         // everytime that we select a different product its editing value will be set to false, (watcher)
         contextMenu: 0,
         selectedProduct: 1,
@@ -48,20 +60,25 @@ document.addEventListener('alpine:init', () => {
             this.products.map((product) => { product.editing = false; })
         },
         selectProduct({id}) {
-            console.log(`selected: ${id}`);
             if (this.selectedProduct !== id) {
                 this.unSelectAll();
             };
             this.selectedProduct = id;
         },
         showContextMenu(id){
-            console.log(`context on: ${id}`);
-            this.contextMenu = id;
+            //change the contextMenu only if its different or if we set it to 0.
+            if (this.selectedProduct === id || id === 0 ) {
+                this.contextMenu = id;
+            }
         },
         enableEdit({id}){
+            clearTimeout(this.focus_timeout);
             this.products.map((product) => {
                 if (id == product.id) {
                     product.editing = true;
+                    this.focus_timeout = setTimeout(() => {
+                        focusDiv('.tag', product.id)
+                    }, 0)
                 }
             });
         },
