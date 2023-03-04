26bd7967062a63fb0cd74187a03730c99d64bba1
PichardoWilfred <pichardowilfred@gmail.com>
2023-03-04 15:28:42 -0400

    removal of products. addition of products. not found products placeholder

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 // add a redo funcitonality
+// ádd the null systems logo on the nav.
 const default_products = [ //default values
     {   id: 1,
         quantity: 0,
@@ -21,21 +22,54 @@ new MiniBar(document.querySelector('#scroll-container'));
 document.addEventListener('alpine:init', () => {
     Alpine.data('store', () => ({
         init() {
-            // this.$watch('selectedProduct', (product, old_prod) => {
-            //     this.products.map((product_) => {
-            //         if (product.id === product_.id) {
-            //             this.selectedProduct.label = product_.label
-            //         }
-            //     });
-            // });
+            // 
+            this.products.map((product_, index) => {
+                if (index === 0) {
+                    this.selectedProduct.id = product_.id
+                }
+                if (this.selectedProduct.id === product_.id) {
+                    this.selectedProduct.label = product_.label
+                }
+            });
         },
         modal: false,
         deleteProduct(id){
+            this.products.forEach((product_, index) => {
+                if (id === product_.id) {
+                    this.products.splice(index, 1);
+                    if (!this.products.length) return; 
+                    // find if there is a product after / before
+                    const { id, label } = this.products[index] || this.products[index - 1] 
+                    this.selectProduct({id, label})
+                }
+            });
+            this.modal = false;
+            this.updateLocalStorage('products', this.products, {serialize: true});
+        },
+        addProduct(){
+            let id
+            if (!this.products.length) {
+                id = 1;
+                this.selectProduct({id, label: '#' +  id})
+            }else {
+                if (this.products[this.products.length + 1]) {
+                    id = this.products[this.products.length + 1].id + 1
+                }else {
+                    id = this.products[this.products.length - 1].id + 1
+                }
+            }
+            this.products.push({
+                id,
+                quantity: 0,
+                label: '#' +  id,
+                editing: false,
+            });
+            this.updateLocalStorage('products', this.products, {serialize: true});
         },
         modalSelected: 'papo',
         // everytime that we select a different product its editing value will be set to false, (watcher)
         contextMenu: 0,
-        selectedProduct: {id: 1, label: '', quantity: 0},
+        selectedProduct: {id: 0, label: '', quantity: 0},
         products: [...products],
         unSelect(id){
             this.products.map((product) => { if(product.id === id) product.editing = false; })
@@ -47,6 +81,7 @@ document.addEventListener('alpine:init', () => {
                 if (this.selectedProduct.id !== id) product.editing = false
             })
             this.selectedProduct.id = id;
+            this.selectedProduct.label = label;
         },
         showDelete(id){ //change the contextMenu only if its different or if we set it to 0.
             this.modal = true;
@@ -63,7 +98,7 @@ document.addEventListener('alpine:init', () => {
                 if (id == product.id) product.editing = true;
             });
         },
-        clickOutside({ id, label }, label_){
+        clickOutside({ id, label }, label_) {
             // handle if its outside the .store
             if (id !== this.selectedProduct.id) return;
             if (id !== this.selectedProduct.id) return;
@@ -80,12 +115,53 @@ document.addEventListener('alpine:init', () => {
             this.unSelect(id)
             this.updateLabel({id, label},label_)
         },
+        inputQuantity: 0,
+        invalidQuantity: false,
+        editQuantity(action){
+            // validate 0
+            if (this.inputQuantity === undefined || this.inputQuantity === null || this.inputQuantity === 0) {
+                this.inputQuantity = 0;
+                this.invalidQuantity = true;
+                return;
+            }
+            if (this.inputQuantity < 0) {
+                this.invalidQuantity = true;
+                return;
+            }
+            
+            this.invalidQuantity = false;
+            const new_quantity = Math.ceil(this.inputQuantity);
+            // console.log(`${action}ing: ${new_quantity} to: ${this.selectedProduct.id} - ${this.selectedProduct.label}`);
+
+            let multiple = 0;
+            switch (action) {
+                case 'add':
+                    multiple = 1;
+                    break;
+                case 'substract':
+                    multiple = -1;
+                    break;
+                default:
+                    console.error('operation unrecognized');
+                    break;
+            }
+            // console.log(multiple);
+            this.products.map((product) => {
+                if (product.id === this.selectedProduct.id) {
+                    product.quantity = product.quantity + (new_quantity * multiple)
+                }
+            });
+            this.updateLocalStorage('products', this.products, {serialize: true});
+            this.inputQuantity = 0;
+            // get the input data
+            // clean the input
+        },
         updateLocalStorage(key, value, options = {serialize: false}){
             if (options.serialize) {
-                console.log('serializing');
+                // console.log('serializing');
                 localStorage.setItem(key, JSON.stringify(value))                
             }else {
-                console.log('not serializing');
+                // console.log('not serializing');
                 localStorage.setItem(key, value)                
             }
         }
