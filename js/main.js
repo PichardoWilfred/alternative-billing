e4196ffb3acb98d9fd21ac06f13e0f3ef3e25bad
Developer <pichardowilfred@gmail.com>
2023-03-16 20:05:13 -0400

    editing label functionality fully supported

--- js/main.js
+++ js/main.js
@@ -4,15 +4,21 @@ const default_tables = [ //default values
     {   id: 1, 
         label: 'Default #1',
         quantities: [20, 30, 50],
-        editing: null,
+        editing: {
+            label: false,
+            quantity: false
+        },
     },
     {   id: 2,
         label: 'Default #2',
         quantities: [10, 280, 20],
-        editing: null,
+        editing: {
+            label: false,
+            quantity: false
+        },
     },
 ]
-const selectedTable = JSON.parse(localStorage.getItem('selectedTable')) || { id: 0};
+const selectedTable = JSON.parse(localStorage.getItem('selectedTable')) || { id: 0, editing: {label: false, quantity: false}};
 
 const tables = JSON.parse(localStorage.getItem('tables')) || default_tables;
 // new MiniBar(document.querySelector('#scroll-container'));
@@ -23,94 +29,106 @@ document.addEventListener('alpine:init', () => {
             quantity: 0,
             focus: 0
         },
+        modal: false,
         init() {
             if (selectedTable.id !== 0) return; //if there's not selectedTable on localstorage assign the first one 
             this.tables.map((table_, index) => {
                 if (index === 0) {
-                    this.selectedTable.id = table_.id
+                    this.selectedTable.id = table_.id;
                 }
                 if (this.selectedTable.id === table_.id) {
                     this.selectedTable.label = table_.label;
                     this.selectedTable.quantities = table_.quantities;
-                    this.selectedTable.editing = null;
+                    
+                    this.selectedTable['editing'].label = false;
+                    this.selectedTable['editing'].quantity = false;
+
                     this.updateLocalStorage('selectedTable', this.selectedTable, { serialize: true });
-                }
+                } 
             });
         },
-        updateLabel(action = 'enter',input){ // rewriting the tables array
-            // console.log(action);
-            if (action === 'unfocused' ) {
-                console.log(input === document.activeElement);
-            }
-            if (action === 'enter') {
-                console.log('uwu');
-            }
-            const label_ = input.value;
-            const { id, label } = this.selectedTable;
+        updateLabel(element) { // rewriting the tables array
+            element.blur();
+            const label_ = element.value;
             const new_label = label_.replace(/\s+/g, ' ').trim();
-            this.selectedTable.label = new_label || label;
-            
-            // this.tables.map((table) => { if (table.id === id) table.label = new_label; })
-            // this.updateLocalStorage('tables', this.tables, {serialize: true});
+            if (new_label) {
+                this.selectedTable.label = new_label || label;
+                this.updateTable({ type: 'label', new_label});
+            }
+            //maybe clean the input
         },
-        enableEdit({ id }){
+        focusLabel() {
+            this.tables.map((table) => {
+                if (table.id === this.selectedTable.id) table.editing.label = true;
+            });
+        },
+        // enableEdit({ id }){
             // this.tables.map((table) => {
             //     if (id == table.id) table.editing = true;
             // });
-        },
-        editingLabel: false,
+        // },
+        // editingLabel: false,
         focus_quantity(index, label) {
-            this.focus = setTimeout(() => { // we need to make this function to enter after our .outside handler (saveQuantity)
-                if (this.selectedTable.editing === index) return; // ignore if we select the same one
-                this.selectedTable.editing = index;
+            this.timeout.focus = setTimeout(() => { // we need to make this function to enter after our .outside handler (saveQuantity)
+                if (this.selectedTable.editing.quantity === index) return; // ignore if we select the same one
+                this.selectedTable.editing.quantity = index;
                 const input = document.querySelector(`#input-quantity-${index}`);
                 this.timeout.quantity = setTimeout(() => {
                     input.focus();
                 }, 0);
-            }, 0)
-        },
+            }, 0);
+        }, 
         saveQuantity(action, index, element) {
-            if (this.selectedTable.editing !== index) return; //validating that only the selected will be triggered
-            const index_editing = this.selectedTable.editing;
-            this.selectedTable.editing = null;
+            if (this.selectedTable.editing.quantity !== index) return; //validating that only the selected will be triggered
+            const index_editing = this.selectedTable.editing.quantity;
+            this.selectedTable.editing.quantity = false;
             let new_quantity;
+            // we need this in case the user clicks on a different tab.
             if (action === 'unfocused') {
                 const input = element.querySelector('input')
                 new_quantity = input.value;
             }
             if (action === 'enter') new_quantity = element.value;
-            this.updateTable(new_quantity, index_editing);
+            this.updateTable({new_quantity, index: index_editing, type: 'quantity'});
         },
-        updateTable(new_quantity, index) {
-            this.tables.map((table) => { // updating the specific quantity
-                if (table.id === this.selectedTable.id) {
-                    table.quantities[index] = new_quantity * 1;
+        // new_quantity = 0, index = 0, action
+        updateTable(action) {
+            let last_edited;  
+            this.tables.map((table) => {
+                if (table.editing.label) {
+                    last_edited = table;
+                    table.editing.label = false;
+                };
+                if (table.id === this.selectedTable.id) {  // updating the specific quantity
+                    if (action.type === 'quantity') {
+                        table.quantities[action.index] = action.new_quantity * 1;
+                        this.selectedTable.quantities = table.quantities;
+                    }
+                    // we also need to be recently focused
+                    if (action.type === 'label' && last_edited) {
+                        table.label = action.new_label;
+                    }
                 }
             });
-            this.tables.map((table_) => { // updating the selectedTable object
-                if (table_.id === this.selectedTable.id) {
-                    this.selectedTable.quantities = table_.quantities;
-                }
-            });
+            
             this.updateLocalStorage('selectedTable', this.selectedTable, { serialize: true })
             this.updateLocalStorage('tables', this.tables, { serialize: true });
         },
-        modal: false,
         deleteTable(id) {
             this.tables.forEach((table_, index) => {
                 if (id === table_.id) {
                     this.tables.splice(index, 1);
                     if (!this.tables.length) return; 
-                    // find if there is a table after / before
+                    // find if there is a table after / before.
                     const { id, label } = this.tables[index] || this.tables[index - 1] 
                     this.selectTable({id, label})
                 }
             });
             this.modal = false;
-            this.updateLocalStorage('tables', this.tables, {serialize: true});
+            this.updateLocalStorage('tables', this.tables, { serialize: true });
         },
-        addTable(){
-            let id
+        addTable() {
+            let id;
             if (!this.tables.length) {
                 id = 1;
                 this.selectTable({id, label: '#' +  id})
@@ -125,9 +143,12 @@ document.addEventListener('alpine:init', () => {
                 id,
                 quantity: 0,
                 label: '#' +  id,
-                editing: false,
+                editing: {
+                    label: false,
+                    quantity: false,
+                },
             });
-            this.updateLocalStorage('tables', this.tables, {serialize: true});
+            this.updateLocalStorage('tables', this.tables, { serialize: true });
         },
         // everytime that we select a different table its editing value will be set to false, (watcher)
         contextMenu: 0,
@@ -135,74 +156,76 @@ document.addEventListener('alpine:init', () => {
         selectedTable: selectedTable,
         tables: [...tables],
         unSelect(id){
-            this.tables.map((table) => { if(table.id === id) table.editing = false; })
+            this.tables.map((table) => { if(table.id === id) {
+                table.editing.label = false;
+                table.editing.quantity = false;
+                
+            } })
         },
         selectTable({ id, label, quantities }) {
             if (this.selectedTable.id !== id) this.unSelect(id);
-            // clean the rest
             this.tables.map((table) => {
-                if (this.selectedTable.id !== id) table.editing = false
+                if (this.selectedTable.id !== id) table.editing.quantity = false; // clean the rest
             })
-            this.selectedTable.id = id;
+            this.selectedTable.id = id; 
             this.selectedTable.label = label;
             this.selectedTable.quantities = quantities;
             
-            this.updateLocalStorage('selectedTable', this.selectedTable, {serialize: true});
+            this.updateLocalStorage('selectedTable', this.selectedTable, { serialize: true });
         },
         focus_newQuantity(index, label) {},
-        clickOutside(index, label_) {
-            // if (id !== this.selectedTable.id) return;
-            // let editing;
-            // this.tables.map((table) => { if(table.id === id) editing = table.editing })
+        // clickOutside(index, label_) {
+        // if (id !== this.selectedTable.id) return;
+        // let editing;
+        // this.tables.map((table) => { if(table.id === id) editing = table.editing })
 
-            // this.unSelect(id);
-            // this.updateLabel({id, label}, label_)
-        },
-        enterPressed({ id, label }, label_){
-            this.unSelect(id)
-            this.updateLabel({id, label}, label_)
-        },
-        inputQuantity: 0,
-        invalidQuantity: false,
-        editQuantity(action){
-            // validate 0
-            if (this.inputQuantity === undefined || this.inputQuantity === null || this.inputQuantity === 0) {
-                this.inputQuantity = 0;
-                this.invalidQuantity = true;
-                return;
-            }
-            if (this.inputQuantity < 0) {
-                this.invalidQuantity = true;
-                return;
-            }
+        // this.unSelect(id);
+        // this.updateLabel({id, label}, label_)
+        // },
+        // enterPressed({ id, label }, label_){
+        //     this.unSelect(id)
+        //     this.updateLabel({id, label}, label_)
+        // },
+        // inputQuantity: 0,
+        // invalidQuantity: false,
+        // editQuantity(action) {
+        //     if (this.inputQuantity === undefined || this.inputQuantity === null || this.inputQuantity === 0) {
+        //         this.inputQuantity = 0;
+        //         this.invalidQuantity = true;
+        //         return;
+        //     }
+        //     if (this.inputQuantity < 0) {
+        //         this.invalidQuantity = true;
+        //         return;
+        //     }
             
-            this.invalidQuantity = false;
-            const new_quantity = Math.ceil(this.inputQuantity);
-            // console.log(`${action}ing: ${new_quantity} to: ${this.selectedTable.id} - ${this.selectedTable.label}`);
+        //     this.invalidQuantity = false;
+        //     const new_quantity = Math.ceil(this.inputQuantity);
+        //     // console.log(`${action}ing: ${new_quantity} to: ${this.selectedTable.id} - ${this.selectedTable.label}`);
 
-            let multiple = 0;
-            switch (action) {
-                case 'add':
-                    multiple = 1;
-                    break;
-                case 'substract':
-                    multiple = -1;
-                    break;
-                default:
-                    console.error('operation unrecognized');
-                    break;
-            }
-            // console.log(multiple);
-            this.tables.map((table) => {
-                if (table.id === this.selectedTable.id) {
-                    table.quantity = table.quantity + (new_quantity * multiple)
-                }
-            });
-            this.updateLocalStorage('tables', this.tables, {serialize: true});
-            this.inputQuantity = 0;
-            // get the input data
-            // clean the input
-        },
+        //     let multiple = 0;
+        //     switch (action) {
+        //         case 'add':
+        //             multiple = 1;
+        //             break;
+        //         case 'substract':
+        //             multiple = -1;
+        //             break;
+        //         default:
+        //             console.error('operation unrecognized');
+        //             break;
+        //     }
+        //     // console.log(multiple);
+        //     this.tables.map((table) => {
+        //         if (table.id === this.selectedTable.id) {
+        //             table.quantity = table.quantity + (new_quantity * multiple)
+        //         }
+        //     });
+        //     this.updateLocalStorage('tables', this.tables, {serialize: true});
+        //     this.inputQuantity = 0;
+        //     // get the input data
+        //     // clean the input
+        // },
         updateLocalStorage(key, value, options = {serialize: false}){
             if (options.serialize) {
                 localStorage.setItem(key, JSON.stringify(value))                
